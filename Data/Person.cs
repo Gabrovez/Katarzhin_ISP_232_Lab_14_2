18be061a902e74926b62b4e8b7956ef340b5c5a0
Gabrovez <gabrovez321@gmail.com>
2025-11-05 19:28:53 +0300

    Шаг 13. ListBox ItemSource

--- Data/Person.cs
+++ Data/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataBinding.Data
 {
-    internal class Person
+    public class Person
     {
         public int Age { get; set; }
         public string Name { get; set; }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,18 +5,18 @@ namespace DataBinding
 {
     public partial class MainWindow : Window
     {
+        public List<Person> People = new List<Person>
+        {
+            new Person {Name = "Grisha", Age= 18},
+            new Person {Name = "Anton", Age= 22},
+            new Person {Name = "Stas", Age= 20},
+            new Person {Name = "Ura", Age= 32}
+
+        };
         public MainWindow()
         {
             InitializeComponent();
-            ListBoxNames.ItemsSource = new List<string>()
-            {
-                "Grisha",
-                "Anton",
-                "Stas",
-                "Ura"
-            };
-            
-
+            ListBoxNames.ItemsSource = People;
         }
     }
 }
