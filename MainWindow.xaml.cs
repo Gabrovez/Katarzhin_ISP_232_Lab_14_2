bba9157ff5881c7d47cd7294c0038f45eb9f2b85
Gabrovez <gabrovez321@gmail.com>
2025-11-05 19:22:20 +0300

    Шаг 12. Введение в ListBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,36 +1,22 @@
 ﻿using DataBinding.Data;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace DataBinding
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        Person person = new Person
-        {
-            Name = "Grisha",
-            Age = 18
-        };
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = person;
-        }
+            ListBoxNames.ItemsSource = new List<string>()
+            {
+                "Grisha",
+                "Anton",
+                "Stas",
+                "Ura"
+            };
+            
 
-        private void ButtonInfo_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show($"{person.Name} is {person.Age} years old.");
         }
     }
 }
